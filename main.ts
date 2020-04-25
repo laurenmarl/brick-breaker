1f82976d6505c8c459c6c4123e648849db68bccf
laurenmarl <62858091+laurenmarl@users.noreply.github.com>
2020-04-24 22:27:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,297 @@
+namespace SpriteKind {
+    export const edge = SpriteKind.create()
+    export const ball = SpriteKind.create()
+    export const top = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.edge, function (sprite, otherSprite) {
+    sprite.setVelocity(-1 * sprite.vx, -1 * sprite.vy)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.top, function (sprite, otherSprite) {
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+})
+sprites.onOverlap(SpriteKind.ball, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.setVelocity((sprite.x - otherSprite.x) * 3, -1 * sprite.vy)
+    if (sprite.vx >= -150) {
+        sprite.vx += -5
+    }
+})
+let startBallVar = 0
+scene.setBackgroundColor(10)
+let paddle = sprites.create(img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`, SpriteKind.Player)
+paddle.setPosition(80, 110)
+controller.moveSprite(paddle, 100, 0)
+paddle.setFlag(SpriteFlag.StayInScreen, true)
+let top = sprites.create(img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`, SpriteKind.top)
+top.setPosition(80, 0)
+let left = sprites.create(img`
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+`, SpriteKind.edge)
+left.setPosition(0, 60)
+let right = sprites.create(img`
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+`, SpriteKind.edge)
+right.setPosition(159, 60)
+let ballVar = sprites.create(img`
+. 1 1 . 
+1 1 1 1 
+1 1 1 1 
+. 1 1 . 
+`, SpriteKind.ball)
+game.onUpdate(function () {
+    if (startBallVar == 0) {
+        ballVar.setPosition(paddle.x, 105)
+        ballVar.setVelocity(0, 0)
+        if (controller.A.isPressed()) {
+            startBallVar = 1
+        }
+    }
+    if (startBallVar == 1) {
+        ballVar.setVelocity(Math.randomRange(-30, 30), -50)
+        startBallVar = 1
+    }
+})
